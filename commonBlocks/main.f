d4ecf158477627e659fde0fde10d2c2001bee2c5
Thomas Boudreaux <thomas@boudreauxmail.com>
2020-11-13 09:47:16 -0500

    Fixed Common Block Typing Error

--- commonBlocks/main.f
+++ commonBlocks/main.f
@@ -3,6 +3,5 @@
         DATA MA/1/, MB/2/, MC/3/
         WRITE(*,*)"In Main Program Main Block is",MA,MB,MC
         CALL SUBA
-        WRITE(*,*)"In Main Program Sub A Block is",ASA,ASB,ASC
         CALL SUBB
        END
--- commonBlocks/subB.f
+++ commonBlocks/subB.f
@@ -1,8 +1,14 @@
        SUBROUTINE SUBB
+        COMMON/MAINBLOCK/MA,MB,MC
         COMMON/SUBBBLOCK/BSA,BSB,BSC
         COMMON/SUBABLOCK/ASA,ASB,ASC
         DATA BSA/10.0/,BSB/20.0/,BSC/30.0/
+        CHARACTER(1)ASA
+        CHARACTER(1)ASB
+        CHARACTER(1)ASC
         WRITE(*,*)"In Subroutine B Local Block is",BSA,BSB,BSC
         WRITE(*,*)"In Subroutine B Main Block is",MA,MB,MC
-        WRITE(*,*)"In Subroutine B A Block is",ASA,ASB,ASC
+        WRITE(*,*)"In Subroutine B A Block is ",ASA," ",ASB," ",ASC
+        CALL SUBA
+        WRITE(*,*)"In Subroutine B A Block is ",ASA," ",ASB," ",ASC
        END

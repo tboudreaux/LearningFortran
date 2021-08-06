e73e11c09a94bbab82506c2d4c04e409cef12167
Thomas Boudreaux <thomas@boudreauxmail.com>
2021-08-06 14:00:15 -0400

    Added fortran low number formating check

---
+++ StringFormating/main.f
@@ -0,0 +1,7 @@
+       program main
+              integer :: i
+              real :: f
+              f = 9.8813129168249309E-324
+              WRITE(*,10) f
+ 10           FORMAT(1X,1P10E16.8)
+       end program main
